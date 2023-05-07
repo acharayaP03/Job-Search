51ddb56434e0e113e1e06120df969f6bc3136a7c
acharayaP03 <acharyaP03@gmail.com>
2023-05-07 22:21:45 +1000

    sub navigation test

---
+++ tests/unit/components/TheSubNavigation.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/vue";
+import TheSubNavigation from "../../../src/components/TheSubNavigation.vue";
+
+describe("TheSubNavigation", () => {
+    describe("when user is on jobs page", () => {
+        it("displays job count", () => {
+            render(TheSubNavigation, {
+                global: {
+                    stubs: {
+                        FontAwesomeIcon: true
+                    }
+                },
+                data() {
+                    return {
+                        onJobResultsPage: true,
+                    }
+                }
+            });
+
+            const jobCount = screen.getByText("1653");
+
+            expect(jobCount).toBeInTheDocument();
+        })
+    })
+
+    describe("when user is not on jobs page", () =>{
+        it("dont NOT displays job count", () => {
+            render(TheSubNavigation, {
+                global: {
+                    stubs: {
+                        FontAwesomeIcon: true
+                    }
+                },
+                data() {
+                    return {
+                        onJobResultsPage: false,
+                    }
+                }
+            });
+
+            const jobCount = screen.queryByText("1653");
+
+            expect(jobCount).not.toBeInTheDocument();
+        })
+    })
+})
