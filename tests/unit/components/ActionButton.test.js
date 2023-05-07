50f347b148233fc6d859c765530794f3a6d3c176
acharayaP03 <acharyaP03@gmail.com>
2023-05-07 21:27:15 +1000

    adding new test

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/vue";
+import ActionButton from "@/components/ActionButton.vue";
+
+describe("Action button", () => {
+    it("renders button name", () =>{
+        render(ActionButton, {
+            props: {
+                text: 'Click me'
+            }
+        });
+
+        const button = screen.getByRole('button', {
+            name: /click me/i
+        });
+
+        expect(button).toBeInTheDocument();
+    })
+
+    it("applies one of the several styles to button", () =>{
+        render(ActionButton, {
+            props: {
+                text: 'Sign in',
+                primary: true
+            }
+        });
+
+        const button = screen.getByRole('button', {
+            name: /sign in/i
+        });
+
+        expect(button).toHaveClass('primary')
+    })
+})
