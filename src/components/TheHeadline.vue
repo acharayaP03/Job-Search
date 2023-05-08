30e2c1ad88edf29119ed142bdaca68e6bdd34984
acharayaP03 <acharyaP03@gmail.com>
2023-05-08 21:39:01 +1000

    adding css to the headline component

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
-    <h2>Find your next job at Trishten Tech</h2>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br>
+      for everyone</h1>
+    <h2 class="text-3xl font-light">Find your next job at Trishten Tech</h2>
   </section>
 </template>
 
@@ -20,6 +23,16 @@ export default {
   beforeUnmount() {
     clearInterval(this.interval)
   },
+  computed:{
+      actionClasses(){
+        return {
+          build: this.action === 'Build',
+          create: this.action === 'Create',
+          design: this.action === 'Design',
+          code: this.action === 'Code'
+        }
+      }
+  },
   methods:{
     changeTitle() {
       this.interval = setInterval(() =>{
@@ -35,5 +48,19 @@ export default {
 </script>
 
 <style scoped>
+.build {
+  color: #1a7338;
+}
 
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025
+}
 </style>
