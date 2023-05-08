7ca925a1497b41d4d070df7d22a951c69a42a3cf
acharayaP03 <acharyaP03@gmail.com>
2023-05-08 22:33:45 +1000

    remove all duplicate vi timers and use it on before each and after each

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -5,37 +5,40 @@ import {nextTick} from "vue";
 
 
 describe("TheHeadline", () => {
-    describe("Vitest playground", () => {
-        it("tracks wheter it has been called", () => {
-            const mockFunction = vi.fn();
-            mockFunction(1)
-            expect(mockFunction).toHaveBeenCalledWith(1);
-        })
-    });
+    // describe("Vitest playground", () => {
+    //     it("tracks wheter it has been called", () => {
+    //         const mockFunction = vi.fn();
+    //         mockFunction(1)
+    //         expect(mockFunction).toHaveBeenCalledWith(1);
+    //     })
+    // });
+
+    beforeEach(() =>{
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
+    })
 
     it("displays introductory action verb", () => {
-        vi.useFakeTimers();
         render(TheHeadline);
 
         const actionPhrase = screen.getByRole("heading", {
             name: /build for everyone/i
         });
         expect(actionPhrase).toBeInTheDocument();
-        vi.useRealTimers();
     });
 
     it("changes action verb at a consistent interval", () => {
-        vi.useFakeTimers();
         const mock = vi.fn();
         vi.stubGlobal("setInterval", mock);
         render(TheHeadline);
 
         expect(mock).toHaveBeenCalled()
-        vi.useRealTimers();
     });
 
     it("swaps action verb after interval", async () => {
-        vi.useFakeTimers();
         render(TheHeadline);
 
         // force processes to next timer ignoring the original setInterval in the component
@@ -48,11 +51,9 @@ describe("TheHeadline", () => {
         });
 
         expect(actionPhrase).toBeInTheDocument()
-        vi.useRealTimers();
     });
 
     it("removes interval when component disappears", () => {
-        vi.useFakeTimers()
         const clearInterval = vi.fn();
         vi.stubGlobal("clearInterval", clearInterval);
 
@@ -60,6 +61,5 @@ describe("TheHeadline", () => {
         unmount();
         expect(clearInterval).toHaveBeenCalled();
         vi.unstubAllGlobals();
-        vi.useRealTimers()
     });
 })
