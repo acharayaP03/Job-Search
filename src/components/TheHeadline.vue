d7ffd4650786cae9420e2ddbe245bb7aaf79a87e
acharayaP03 <acharyaP03@gmail.com>
2023-05-08 21:54:58 +1000

    utilizing the util function

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import nextElementInList from "../utils/nextElementInList";
 export default {
   name: "TheHeadline",
   data(){
@@ -37,10 +38,7 @@ export default {
     changeTitle() {
       this.interval = setInterval(() =>{
         const actions = ["Build", "Create", "Design", "Code"];
-        const currentActionIndex = actions.indexOf(this.action);
-        const nextActionIndex = (currentActionIndex + 1) % 4;
-        const nextAction = actions[nextActionIndex];
-        this.action = nextAction
+        this.action = nextElementInList(actions, this.action)
       }, 3000)
     }
   }
