e323b9b776cdd0a71da4640647ac722e70628283
acharayaP03 <acharyaP03@gmail.com>
2023-05-08 22:16:48 +1000

    testing headline component and its setInterval function

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -2,6 +2,7 @@ import { render, screen} from "@testing-library/vue";
 
 import TheHeadline from "../../../src/components/TheHeadline.vue";
 
+
 describe("TheHeadline", () => {
     describe("Vitest playground", () => {
         it("tracks wheter it has been called", () => {
@@ -9,5 +10,26 @@ describe("TheHeadline", () => {
             mockFunction(1)
             expect(mockFunction).toHaveBeenCalledWith(1);
         })
+    });
+
+    it("displays introductory action verb", () => {
+        vi.useFakeTimers();
+        render(TheHeadline);
+
+        const actionPhrase = screen.getByRole("heading", {
+            name: /build for everyone/i
+        });
+        expect(actionPhrase).toBeInTheDocument();
+        vi.useRealTimers();
+    });
+
+    it("changes action verb at a consistent interval", () => {
+        vi.useFakeTimers();
+        const mock = vi.fn();
+        vi.stubGlobal("setInterval", mock);
+        render(TheHeadline);
+
+        expect(mock).toHaveBeenCalled()
+        vi.useRealTimers();
     })
 })
