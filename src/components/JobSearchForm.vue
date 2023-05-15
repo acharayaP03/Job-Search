5009f412f53cc67473afa851971ad645b648e92d
acharayaP03 <acharyaP03@gmail.com>
2023-05-15 22:24:51 +1000

    form setup

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <form class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3">
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3"></font-awesome-icon>
+  </form>
+</template>
+
+<script>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+export default {
+  name: "JobSearchForm",
+  components: {FontAwesomeIcon}
+}
+</script>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,10 +1,11 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20">
+    <section class="flex flex-col h-screen pt-40 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"/>
         <div class="col-span-5 col-start-2">
           <the-headline/>
+          <job-search-form/>
         </div>
         <div class="col-span-5 col-start-7"/>
         <div class="col-span-1 col-start-12"/>
@@ -15,10 +16,12 @@
 
 <script>
 import TheHeadline from "./TheHeadline.vue";
+import JobSearchForm from "./JobSearchForm.vue";
 export default {
   name: "TheHero",
   components: {
-    TheHeadline
+    TheHeadline,
+    JobSearchForm
   }
 
 }
