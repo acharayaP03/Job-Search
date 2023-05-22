ec3a52971213a4da38a2c4b697588945f09facf9
acharayaP03 <acharyaP03@gmail.com>
2023-05-22 22:59:12 +1000

    create component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-full flex items-center text-3xl">
+    <h1 class="font-light">Home view</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView"
+}
+</script>
---
+++ src/views/JobSearchResult.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-full flex items-center text-3xl">
+    <h1 class="font-light">Search Result</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobSearchResult"
+}
+</script>
