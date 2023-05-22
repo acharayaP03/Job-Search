217081102c4d170b3a3fec2a4675804fb5051264
acharayaP03 <acharyaP03@gmail.com>
2023-05-22 23:55:22 +1000

    replace data property with computed to show shub nav only on job search

--- tests/unit/components/navigation/TheSubNavigation.test.js
+++ tests/unit/components/navigation/TheSubNavigation.test.js
@@ -1,20 +1,22 @@
 import { render, screen } from "@testing-library/vue";
-import TheSubNavigation from "../../../../src/components/Navigation/TheSubNavigation.vue";
+import TheSubNavigation from "@/components/Navigation/TheSubNavigation.vue";
 
 describe("TheSubNavigation", () => {
     describe("when user is on jobs page", () => {
         it("displays job count", () => {
+            const $route = {
+                name: "JobResults",
+            }
+
             render(TheSubNavigation, {
                 global: {
+                    mocks: {
+                        $route
+                    },
                     stubs: {
                         FontAwesomeIcon: true
                     }
                 },
-                data() {
-                    return {
-                        onJobResultsPage: true,
-                    }
-                }
             });
 
             const jobCount = screen.getByText("1653");
@@ -25,8 +27,15 @@ describe("TheSubNavigation", () => {
 
     describe("when user is not on jobs page", () =>{
         it("dont NOT displays job count", () => {
+
+            const $route = {
+                name: "Home"
+            }
             render(TheSubNavigation, {
                 global: {
+                    mocks: {
+                        $route
+                    },
                     stubs: {
                         FontAwesomeIcon: true
                     }
