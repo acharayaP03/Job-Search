3af294272c503e0b6a64419c8e0c3908ed68c9c9
acharayaP03 <acharyaP03@gmail.com>
2023-05-27 20:50:01 +1000

    Job listing

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,10 +1,19 @@
 <template>
-<h1>Job view - {{ getJobId }}</h1>
+
+  <div class="flex flex-row flex-nowrap w-full">
+    <job-filters-sidebar/>
+    <job-listings/>
+  </div>
 </template>
 
 <script>
+import {JobFiltersSidebar, JobListings} from "../components/JobResults";
 export default {
   name: "JobView",
+  components:{
+    JobListings,
+    JobFiltersSidebar
+  },
   computed:{
     getJobId() {
       return this.$route.params.id
