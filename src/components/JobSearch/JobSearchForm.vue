cd792814f018eed836890ddb1947a91c680c95af
acharayaP03 <acharyaP03@gmail.com>
2023-05-27 18:25:29 +1000

    page navigation with query params

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -3,19 +3,19 @@
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3"></font-awesome-icon>
     <div class="flex flex-1 flex-nowrap h-full text-base font-light">
       <div class="flex h-full flex-1 relative items-center pr-3">
-        <label class="absolute left-0 -top-10">Role</label>
+        <label for="role" class="absolute left-0 -top-10">Role</label>
         <!--
           when child TextInput component emits handleInput with event, the pay load will be passed to
           parent component, where we can capture directly on $event rather than declaring methods and assigning it to
           reactive data in the methods.
           we can directly assign like below. this is called inline expression
         -->
-        <text-input placeholder="Software engineer" v-model="role"/>
+        <text-input id="role" placeholder="Software engineer" v-model="role"/>
       </div>
       <span class="flex items-center h-full border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3">in</span>
       <div class="flex h-full flex-1 relative items-center pl-3">
-        <label class="absolute left-0 -top-10">Where?</label>
-        <text-input v-model="location" placeholder="Sydney" />
+        <label for="location" class="absolute left-0 -top-10">Where?</label>
+        <text-input id="location" v-model="location" placeholder="Sydney" />
       </div>
     </div>
 
