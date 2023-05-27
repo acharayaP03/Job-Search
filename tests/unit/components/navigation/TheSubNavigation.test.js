192fe366e405bce8acdece3c4c5d721a9c768b09
acharayaP03 <acharyaP03@gmail.com>
2023-05-27 16:20:40 +1000

    navigation test refactor with factory function

--- tests/unit/components/navigation/TheSubNavigation.test.js
+++ tests/unit/components/navigation/TheSubNavigation.test.js
@@ -2,23 +2,25 @@ import { render, screen } from "@testing-library/vue";
 import TheSubNavigation from "@/components/Navigation/TheSubNavigation.vue";
 
 describe("TheSubNavigation", () => {
-    describe("when user is on jobs page", () => {
-        it("displays job count", () => {
-            const $route = {
-                name: "JobResults",
-            }
-
-            render(TheSubNavigation, {
-                global: {
-                    mocks: {
-                        $route
-                    },
-                    stubs: {
-                        FontAwesomeIcon: true
+    const renderTheSubNav = (routeName) => {
+        render(TheSubNavigation, {
+            global: {
+                mocks: {
+                    $route : {
+                        name: routeName
                     }
                 },
-            });
+                stubs: {
+                    FontAwesomeIcon: true
+                }
+            },
+        });
+    }
+    describe("when user is on jobs page", () => {
+        it("displays job count", () => {
+            const routeName = "JobResults"
 
+            renderTheSubNav(routeName)
             const jobCount = screen.getByText("1653");
 
             expect(jobCount).toBeInTheDocument();
@@ -28,27 +30,10 @@ describe("TheSubNavigation", () => {
     describe("when user is not on jobs page", () =>{
         it("dont NOT displays job count", () => {
 
-            const $route = {
-                name: "Home"
-            }
-            render(TheSubNavigation, {
-                global: {
-                    mocks: {
-                        $route
-                    },
-                    stubs: {
-                        FontAwesomeIcon: true
-                    }
-                },
-                data() {
-                    return {
-                        onJobResultsPage: false,
-                    }
-                }
-            });
+            const routeName = "Home";
+            renderTheSubNav(routeName)
 
             const jobCount = screen.queryByText("1653");
-
             expect(jobCount).not.toBeInTheDocument();
         })
     })
