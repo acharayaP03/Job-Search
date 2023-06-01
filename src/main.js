01f3b167ae396091979eb16528de29d298e70bad
acharayaP03 <acharyaP03@gmail.com>
2023-06-01 21:08:29 +1000

    create and use store

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
+import {createPinia} from "pinia";
 
 import '@/index.css';
 import App from './App.vue';
 import router from './router';
+
+const pinia = createPinia();
 
 library.add(faSearch);
 
