c447a5334f3c4b94a5ace6a62edcb6340e692c95
acharayaP03 <acharyaP03@gmail.com>
2023-06-04 21:41:30 +1000

    Create resuable header container

--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -1,8 +1,10 @@
 import CollapsibleAccordion from "./CollapsibleAccordion.vue";
 import ActionButton from "./ActionButton.vue";
 import TextInput from "./TextInput.vue";
+import HeaderContainer from "./HeaderContainer.vue";
 export {
     CollapsibleAccordion,
     ActionButton,
-    TextInput
+    TextInput,
+    HeaderContainer
 }
