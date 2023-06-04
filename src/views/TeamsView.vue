edf6239212fd6d968e560955e0faa4b111dfdb27
acharayaP03 <acharyaP03@gmail.com>
2023-06-04 21:41:59 +1000

    create team page

---
+++ src/views/TeamsView.vue
@@ -0,0 +1,11 @@
+<template>
+  <header-container/>
+</template>
+
+<script>
+import {HeaderContainer} from "../components/Shared";
+export default {
+  name: "TeamsView",
+  components: {HeaderContainer}
+}
+</script>
