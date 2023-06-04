9689e72ca977a782307766c2a86876b93c28d106
acharayaP03 <acharyaP03@gmail.com>
2023-06-04 20:00:46 +1000

    use of slots

--- src/components/JobResults/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar.vue
@@ -7,7 +7,30 @@
           <action-button text="Clear Filters" button-type="secondary" />
         </div>
       </div>
-      <collapsible-accordion/>
+      <collapsible-accordion>
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="h-8 w-1/2">
+                <input type="checkbox" id="vueTube" class="mr-3">
+                <label for="vueTube">Vue tube</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input type="checkbox" id="vue and me" class="mr-3">
+                <label for="vue and me">Vue and me</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input type="checkbox" id="Et Vue Brute" class="mr-3">
+                <label for="Et Vue Brute">Et Vue Brute</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input type="checkbox" id="Vue and half men" class="mr-3">
+                <label for="Vue and half men">Vue and half men</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </collapsible-accordion>
     </section>
   </div>
 </template>
