32406e01dc6aebb0d916b6dfe58eda42098ceacb
acharayaP03 <acharyaP03@gmail.com>
2023-06-04 21:50:48 +1000

    named slots

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,13 @@
 <template>
-  <header-container/>
+  <header-container>
+    <template v-slot:title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+
+    <template v-slot:subtitle>
+      <h2 class="my-4 w-full text-base font-light">Its awesome working here. Why don't you come join us</h2>
+    </template>
+  </header-container>
 </template>
 
 <script>
