fb1b69b91b3d7734fd725ec9e58189ba80e99a87
acharayaP03 <acharyaP03@gmail.com>
2023-06-11 20:20:45 +1000

    spot light section

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex flex-col h-screen pt-40 pb-20">
+  <main class="flex flex-col h-screen ">
+    <section class="pt-40 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-1 col-start-1"/>
         <div class="col-span-5 col-start-2">
@@ -16,17 +16,47 @@
         <div class="col-span-1 col-start-12"/>
       </div>
     </section>
+    <spot-lights class="flex flex-row justify-center pb-16">
+      <template v-slot:default="{ image, title, description, index }">
+        <router-link v-if="index !==1 " to="/jobs/results" class="mx-5 flex flex h-[268px] w-[380px] flex-col rounded-lg border bg-brand-gray-2 overflow-hidden">
+          <img :src="image" :alt="title" class="h-2/4 w-full object-cover">
+          <div class="mt-3 px-6 py-2">
+            <h3>{{ title }}</h3>
+            <p class="mt-3 text-sm truncate">{{ description }}</p>
+          </div>
+
+          <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-brand-blue-1">See jobs</router-link>
+        </router-link>
+
+        <router-link v-if="index === 1" to="/jobs/results" class="mx-5 flex h-[268px] w-[530px] flex-col rounded-lg border bg-brand-gray-2 overflow-hidden">
+          <div
+              :style="{ backgroundImage: 'url(' + image + ')', backgroundSize: '100%'}"
+              class="flex h-full flex-col justify-end bg-cover"
+          >
+            <div class="text-white">
+              <div class="px-6 py-4">
+                <h3 class="text-xl">{{ title }}</h3>
+                <p class="pt-4">{{ description }}</p>
+              </div>
+            </div>
+            <router-link to="/jobs/results" class="px-6 pb-4 text-sm text-white">See jobs</router-link>
+          </div>
+        </router-link>
+      </template>
+    </spot-lights>
   </main>
 </template>
 
 <script>
 import TheHeadline from "./TheHeadline.vue";
 import JobSearchForm from "./JobSearchForm.vue";
+import SpotLights from "./SpotLights.vue";
 export default {
   name: "TheHero",
   components: {
     TheHeadline,
-    JobSearchForm
+    JobSearchForm,
+    SpotLights
   }
 
 }
