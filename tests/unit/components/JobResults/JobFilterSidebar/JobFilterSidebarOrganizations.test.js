c9741b36eea7e869b9fdf5e0e74ef4761fed4ad5
acharayaP03 <acharyaP03@gmail.com>
2023-06-12 20:31:46 +1000

    component testing

---
+++ tests/unit/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
+import {createPinia, createTestingPinia} from "@pinia/testing";
+
+import JobFiltersSidebarOrganization from "../../../../../src/components/JobResults/JobFiltersSidebarOrganization.vue";
+import { useJobsStore } from "../../../../../src/stores/jobs";
+
+describe("JobFilterSidebarOrganization", () => {
+    it("renders unique list of organizations from jobs", async () => {
+        const pinia = createTestingPinia();
+        const jobsStore = useJobsStore();
+        jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+
+        render(JobFiltersSidebarOrganization, {
+            global: {
+                plugins: [pinia],
+                stubs: {
+                    FontAwesomeIcon : true
+                }
+            }
+        });
+
+        const button = screen.getByRole('button', {
+            name: /organizations/i
+        })
+
+        await userEvent.click(button);
+
+        const organizationListItems = screen.getAllByRole("listitem");
+        const organizations = organizationListItems.map((node) => node.textContent);
+
+        expect(organizations).toEqual(["Google", "Amazon"])
+    })
+})
