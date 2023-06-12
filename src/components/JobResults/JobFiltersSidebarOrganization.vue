d3fcabb1e95f001878244fff71ee01b3c5f85e00
acharayaP03 <acharyaP03@gmail.com>
2023-06-12 20:31:06 +1000

    use getters to component

--- src/components/JobResults/JobFiltersSidebarOrganization.vue
+++ src/components/JobResults/JobFiltersSidebarOrganization.vue
@@ -3,21 +3,9 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-row flex-wrap">
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="vueTube" class="mr-3">
-            <label for="vueTube">Vue tube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="vue and me" class="mr-3">
-            <label for="vue and me">Vue and me</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="Et Vue Brute" class="mr-3">
-            <label for="Et Vue Brute">Et Vue Brute</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="Vue and half men" class="mr-3">
-            <label for="Vue and half men">Vue and half men</label>
+          <li class="h-8 w-1/2" v-for="organization in UNIQUE_ORGANIZATIONS" :key="organization">
+            <input type="checkbox" :id="organization" class="mr-3">
+            <label :for="organization">{{ organization }}</label>
           </li>
         </ul>
       </fieldset>
@@ -26,11 +14,16 @@
 </template>
 
 <script>
+import { mapState } from "pinia";
+import { useJobsStore, UNIQUE_ORGANIZATIONS } from "../../stores/jobs";
 import { CollapsibleAccordion } from '@/components/Shared'
 export default {
-  name: "JobFiltersSidebarOraganization",
+  name: "JobFiltersSidebarOrganization",
   components:{
     CollapsibleAccordion
+  },
+  computed:{
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS])
   }
 }
 </script>
