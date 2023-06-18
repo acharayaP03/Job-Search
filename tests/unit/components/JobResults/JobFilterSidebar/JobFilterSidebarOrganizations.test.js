4022869940974e3d25db4f0a7714e2df5d9c3323
acharayaP03 <acharyaP03@gmail.com>
2023-06-18 19:08:40 +1000

    Refactor Test

--- tests/unit/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.test.js
+++ tests/unit/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.test.js
@@ -7,10 +7,10 @@ import { useJobsStore } from "../../../../../src/stores/jobs";
 import {useUserStore} from "../../../../../src/stores/user";
 
 describe("JobFilterSidebarOrganization", () => {
-    it("renders unique list of organizations from jobs", async () => {
+    const renderJobFilterSidebarOrganizations = () => {
         const pinia = createTestingPinia();
         const jobsStore = useJobsStore();
-        jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+        const userStore = useUserStore();
 
         render(JobFiltersSidebarOrganization, {
             global: {
@@ -20,6 +20,13 @@ describe("JobFilterSidebarOrganization", () => {
                 }
             }
         });
+
+        return { jobsStore, userStore }
+    }
+    it("renders unique list of organizations from jobs", async () => {
+
+        const { jobsStore } = renderJobFilterSidebarOrganizations();
+        jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
 
         const button = screen.getByRole('button', {
             name: /organizations/i
@@ -34,20 +41,8 @@ describe("JobFilterSidebarOrganization", () => {
     });
 
     it("communicates that user has selected checkbox for organization", async () => {
-        const pinia = createTestingPinia();
-        const jobsStore = useJobsStore();
-        const userStore = useUserStore();
-
+        const { jobsStore, userStore } = renderJobFilterSidebarOrganizations()
         jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
-
-        render(JobFiltersSidebarOrganization, {
-            global: {
-                plugins: [pinia],
-                stubs: {
-                    FontAwesomeIcon : true
-                }
-            }
-        });
 
         const button = screen.getByRole('button', {
             name: /organizations/i
