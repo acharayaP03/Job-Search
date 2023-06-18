c5358d4973f0ec26984e7d68db8341a233c95fc8
acharayaP03 <acharyaP03@gmail.com>
2023-06-18 21:45:25 +1000

    passing args to getters

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -9,6 +9,9 @@ export const FILTERED_JOBS_BY_ORGANIZATIONS = 'FILTERED_JOBS_BY_ORGANIZATIONS';
 export const UNIQUE_JOB_TYPES = 'UNIQUE_JOB_TYPES';
 export const FILTERED_JOBS_BY_JOB_TYPES = 'FILTERED_JOBS_BY_JOB_TYPES';
 
+export const INCLUDE_JOB_BY_ORGANIZATION = 'INCLUDE_JOB_BY_ORGANIZATION';
+export const INCLUDE_JOB_BY_JOB_TYPE = 'INCLUDE_JOB_BY_JOB_TYPE';
+
 export const useJobsStore = defineStore("jobs", {
     state: () => ({
         jobs: []
@@ -46,21 +49,18 @@ export const useJobsStore = defineStore("jobs", {
 
             return state.jobs.filter((job) => userStore.selectedJobTypes.includes(job.jobType))
         },
-        [FILTERED_JOBS](state){
+        [INCLUDE_JOB_BY_ORGANIZATION]: () => (job) => {
             const userStore = useUserStore();
-
-            const noSelectedOrganization = userStore.selectedOrganizations.length === 0;
-            const noSelectedJobTypes = userStore.selectedJobTypes.length === 0;
-
-            if(noSelectedOrganization && noSelectedJobTypes ) return state.jobs;
-
-            return state.jobs.filter((job) => {
-                if(noSelectedOrganization) return true
-                return userStore.selectedOrganizations.includes(job.organization)
-            }).filter((job) => {
-                if(noSelectedJobTypes) return true
-                return userStore.selectedJobTypes.includes(job.jobType)
-            })
+            if(userStore.selectedOrganizations.length === 0) return true;
+            return userStore.selectedOrganizations.includes(job.organization)
+        },
+        [INCLUDE_JOB_BY_JOB_TYPE]: () => (job) => {
+            const userStore = useUserStore();
+            if(userStore.selectedJobTypes.length === 0) return true
+            return userStore.selectedJobTypes.includes(job.jobType)
+        },
+        [FILTERED_JOBS](state){
+            return state.jobs.filter((job) => this.INCLUDE_JOB_BY_ORGANIZATION(job)).filter((job) => this.INCLUDE_JOB_BY_JOB_TYPE(job))
         }
     }
 })
