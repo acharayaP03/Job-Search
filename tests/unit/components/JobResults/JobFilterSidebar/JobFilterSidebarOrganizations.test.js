fabc708b5a2e68e62ac34d4c3693135ffbacd27f
acharayaP03 <acharyaP03@gmail.com>
2023-06-18 19:02:53 +1000

    selected organization checkbox test

--- tests/unit/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.test.js
+++ tests/unit/components/JobResults/JobFilterSidebar/JobFilterSidebarOrganizations.test.js
@@ -4,6 +4,7 @@ import {createPinia, createTestingPinia} from "@pinia/testing";
 
 import JobFiltersSidebarOrganization from "../../../../../src/components/JobResults/JobFiltersSidebarOrganization.vue";
 import { useJobsStore } from "../../../../../src/stores/jobs";
+import {useUserStore} from "../../../../../src/stores/user";
 
 describe("JobFilterSidebarOrganization", () => {
     it("renders unique list of organizations from jobs", async () => {
@@ -30,5 +31,35 @@ describe("JobFilterSidebarOrganization", () => {
         const organizations = organizationListItems.map((node) => node.textContent);
 
         expect(organizations).toEqual(["Google", "Amazon"])
+    });
+
+    it("communicates that user has selected checkbox for organization", async () => {
+        const pinia = createTestingPinia();
+        const jobsStore = useJobsStore();
+        const userStore = useUserStore();
+
+        jobsStore.UNIQUE_ORGANIZATIONS = new Set(["Google", "Amazon"]);
+
+        render(JobFiltersSidebarOrganization, {
+            global: {
+                plugins: [pinia],
+                stubs: {
+                    FontAwesomeIcon : true
+                }
+            }
+        });
+
+        const button = screen.getByRole('button', {
+            name: /organizations/i
+        })
+        await userEvent.click(button);
+
+        const googleCheckbox = screen.getByRole("checkbox", {
+            name: /google/i
+        });
+
+        await userEvent.click(googleCheckbox);
+
+        expect(userStore.ADD_SELECTED_ORGANIZATIONS).toHaveBeenCalledWith(['Google'])
     })
 })
