ea4161fbb02e186fa88310819d98ca32a5eb6e14
acharayaP03 <acharyaP03@gmail.com>
2023-07-08 13:52:59 +1000

    refactor with typescript

--- tests/unit/composables/usePreviousAndNextPages.test.ts
+++ tests/unit/composables/usePreviousAndNextPages.test.ts
@@ -1,10 +1,11 @@
+import { ref } from "vue"
 import usePreviousAndNextPages from "../../../src/composables/usePreviousAndNextPages";
 
 
 describe("usePreviousAndNextPages", () => {
     it("calculates page before current one", () => {
-        const currentPage = { value: 8 };
-        const maxPage = { value: 10 };
+        const currentPage = ref(8)
+        const maxPage = ref(10);
 
         const { previousPage } = usePreviousAndNextPages(currentPage, maxPage);
         expect(previousPage.value).toBe(7)
@@ -13,8 +14,8 @@ describe("usePreviousAndNextPages", () => {
 
     describe("when current page is the first page", () => {
         it("does not provide previous page", () => {
-            const currentPage = { value: 1 };
-            const maxPage = { value: 1 };
+            const currentPage = ref(1);
+            const maxPage = ref(1);
 
             const { previousPage } = usePreviousAndNextPages(currentPage, maxPage);
             expect(previousPage.value).toBeUndefined()
@@ -22,8 +23,8 @@ describe("usePreviousAndNextPages", () => {
     })
 
     it("calculates page after current one", () => {
-        const currentPage = { value: 8 };
-        const maxPage = { value: 10 };
+        const currentPage = ref(8);
+        const maxPage = ref(10);
 
         const { nextPage } = usePreviousAndNextPages(currentPage, maxPage);
         expect(nextPage.value).toBe(9)
@@ -31,8 +32,8 @@ describe("usePreviousAndNextPages", () => {
 
     describe("when current page is the last page", () => {
         it("does not provide previous page", () => {
-            const currentPage = { value: 8 };
-            const maxPage = { value: 8 };
+            const currentPage = ref(8);
+            const maxPage = ref(10);
 
             const { nextPage } = usePreviousAndNextPages(currentPage, maxPage);
             expect(nextPage.value).toBeUndefined()
